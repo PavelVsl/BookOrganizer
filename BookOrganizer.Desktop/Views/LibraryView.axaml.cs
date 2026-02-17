4fc0d7020eaae3497028fa8a88273a441f492e8b
Pavel Veselý <pavel@vesely.dev>
2026-02-17 19:32:49 +0100

    Revert "feat(desktop): add Check Source button to detect library duplicates in source folder"
    This reverts commit d0198a10a4c2a439c56d906143fcd4bfeca4e421.

--- BookOrganizer.Desktop/Views/LibraryView.axaml.cs
+++ BookOrganizer.Desktop/Views/LibraryView.axaml.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Globalization;
 using Avalonia.Controls;
-using Avalonia.Data.Converters;
 
 namespace BookOrganizer.Desktop.Views;
 
@@ -11,15 +8,4 @@ public partial class LibraryView : UserControl
     {
         InitializeComponent();
     }
-}
-
-public class BoolToStatusConverter : IValueConverter
-{
-    public static readonly BoolToStatusConverter Instance = new();
-
-    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-        => value is true ? "\u2713 In library" : "\u2717 New";
-
-    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
-        => throw new NotSupportedException();
 }
